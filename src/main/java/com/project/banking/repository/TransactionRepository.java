cee5157dd7cbc5f04c97b065ba6417239d28f015
VIVEK AGRAWAL <agrawal.vivek2017@gmail.com>
2024-09-20 22:38:11 -0500

    cleaning

--- src/main/java/com/project/banking/repository/TransactionRepository.java
+++ src/main/java/com/project/banking/repository/TransactionRepository.java
@@ -1,6 +1,5 @@
 package com.project.banking.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
