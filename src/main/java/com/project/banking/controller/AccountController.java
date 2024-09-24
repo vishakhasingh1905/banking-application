b30286fb8faeef25945534f89a512fa71084d3ee
vagrawal36 <contact.agrawalvivek@gmail.com>
2024-09-24 10:31:22 -0500

    Allowing CrossOrigin for 3000

--- src/main/java/com/project/banking/controller/AccountController.java
+++ src/main/java/com/project/banking/controller/AccountController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +18,9 @@ import com.project.banking.dto.AccountDto;
 import com.project.banking.dto.TransactionDto;
 import com.project.banking.dto.TransferRequestDto;
 import com.project.banking.service.AccountService;
-
 @RestController
 @RequestMapping("/api/accounts")
+@CrossOrigin(origins = "http://localhost:3000")
 public class AccountController {
     @Autowired
     private AccountService accountService;
