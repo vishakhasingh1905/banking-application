9975e842331156d398913962a17f1b9468230c23
vishakhasingh <vishakha.singh1905@gmail.com>
2025-01-18 16:43:28 -0600

    Added SignIn Functionality

---
+++ src/main/java/com/project/banking/dto/SignInRequest.java
@@ -0,0 +1,14 @@
+package com.project.banking.dto;
+import lombok.*;
+
+
+@Setter
+@Getter
+public class SignInRequest {
+    // Getters and Setters
+    private String username;
+    private String password;
+
+}
+
+
--- src/main/java/com/project/banking/repository/CredentialRepository.java
+++ src/main/java/com/project/banking/repository/CredentialRepository.java
@@ -4,7 +4,10 @@ import com.project.banking.entity.Credentials;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CredentialRepository extends MongoRepository<Credentials, String> {
+    Optional<Credentials> findByUsername(String username);
 }
 
